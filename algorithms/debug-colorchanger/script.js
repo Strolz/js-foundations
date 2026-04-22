c118bc5a249dac8f373b3523d6bcb09d94f03bbc
Anton Strolz <tonystrolz@gmail.com>
2026-04-22 12:26:34 -0500

    fix: Complete debug-colorchanger lab
    - finished all user stories correcting all bugs
    - add intention driven debug comments to JS

--- algorithms/debug-colorchanger/script.js
+++ algorithms/debug-colorchanger/script.js
@@ -15,22 +15,27 @@ const darkColorsArr = [
     // You should fix the ReferenceError thrown when the math object is referenced inside the getRandomIndex function
     // You should round down randomIndex to the nearest whole number.
     const randomIndex = Math.floor(Math.random() * darkColorsArr.length);
+
     return randomIndex;
   }
 
-  console.log(getRandomIndex())
-  
-  const body = document.queryselector("body");
-  const bgHexCodeSpanElement = document.querySelector("bg-hex-code");
+  // You should update the value of the body variable to use the correct method name and fix the related TypeError.
+  const body = document.querySelector("body");
+
+  // You should fix bgHexCodeSpanElement so that it correctly selects the element with the id of bg-hex-code.
+  const bgHexCodeSpanElement = document.getElementById("bg-hex-code");
   console.log(bgHexCodeSpanElement)
   
+  // You should modify the color variable inside changeBackgroundColor so that it is set to a random color from the darkColorsArr array.
   function changeBackgroundColor() {
-    const color = darkColorsArr[getRandomIndex];
+    const color = darkColorsArr[getRandomIndex()];
   
     bgHexCodeSpanElement.innerText = color;
     body.style.backgroundColor = color;
   }
-  const btn = document.querySelector("#click-btn");
-  console.log(btn)
-  
-  btn.addEventListener("click", changeBackgroundColor());
+
+  // You should update the btn variable so that it targets the button element. Open up the index.html to see the correct id name for that button element.
+  const btn = document.querySelector(".btn");
+
+  // You should fix the line that adds an event listener for the click event to btn by passing a reference to the changeBackgroundColor function to the method.
+  btn.addEventListener("click", changeBackgroundColor);
