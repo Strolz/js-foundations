db1f954fb5e446bec2bb2f372a47d5971be975b3
Anton Strolz <tonystrolz@gmail.com>
2026-04-27 21:18:28 -0500

    feat: Complete building palindrome-checker algorithm
    - set root CSS styling and set background color
    - set button color and center layout in viewport
    - standardize input and complete JS reverse check palindrome logic
    - add intention driven comments to HTML & JS

--- algorithms/palindrome-checker/script.js
+++ algorithms/palindrome-checker/script.js
@@ -1,4 +1,15 @@
+// Cache DOM elements
 const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
 const resultMsg = document.getElementById("result");
 
+// Validate & standardize input, update UI
+checkBtn.addEventListener("click", () => {
+    if(textInput.value == "") return alert("Please input a value");
+    const cleaned = textInput.value.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+    resultMsg.textContent = cleaned == cleaned.split("").reverse().join("")
+    ? `${textInput.value} is a palindrome`
+    : `${textInput.value} is not a palindrome`
+
+})
