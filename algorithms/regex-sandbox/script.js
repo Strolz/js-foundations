7212080684ba55ca615a778264820215e4e95628
Anton Strolz <tonystrolz@gmail.com>
2026-04-29 22:12:23 -0500

    feat: Complete regex-sandbox algorithm
    - complete new regex expression based on user input
    - enable regex test button to control UI state change
    - complete matching character highlight logic
    - add intention driven comments to HTML & JS

--- algorithms/regex-sandbox/script.js
+++ algorithms/regex-sandbox/script.js
@@ -1,3 +1,4 @@
+// Cache DOM elements
 const regexPattern = document.getElementById("pattern");
 const stringToTest = document.getElementById("test-string");
 const testButton = document.getElementById("test-btn");
@@ -5,26 +6,40 @@ const testResult = document.getElementById("result");
 const caseInsensitiveFlag = document.getElementById("i");
 const globalFlag = document.getElementById("g");
 
+// Verify selected regex flags by user
 function getFlags() {
     let flags = "";
 
-    if(caseInsensitiveFlag.checked == true) {
-    flags += "i"
+    if (caseInsensitiveFlag.checked == true) {
+        flags += "i"
     }
-    if(globalFlag.checked == true) {
-    flags += "g"
+    if (globalFlag.checked == true) {
+        flags += "g"
     }
     return flags;
 }
 
+// Compile RegExp object from user pattern and selected flags
 function buildRegex() {
     let newRegex = regexPattern.value;
     let flagString = getFlags();
     return RegExp(newRegex, flagString);
 }
 
+// Run the regex against the input, show matches or "no match", then highlight them
 function testString() {
-    
+    const regexTest = buildRegex();
+    if(!regexTest.test(stringToTest.innerText)) return testResult.innerText = `no match`
+    testResult.innerText = stringToTest.textContent.match(regexTest);
+    highLightMatches();
 }
 
-testButton.addEventListener("click", () => buildRegex())
+function highLightMatches() {
+    const regex = buildRegex();
+    stringToTest.innerHTML = stringToTest.textContent.replace(regex, match => {
+    return `<span class="highlight">${match}</span>`
+});
+}
+
+// Trigger regex test when the user clicks the button
+testButton.addEventListener("click", () => testString())
