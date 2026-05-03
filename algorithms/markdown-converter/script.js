2e718bbda24e7f1d9a5b60979c0a1b0a3a2865ac
Anton Strolz <tonystrolz@gmail.com>
2026-05-03 16:04:28 -0500

    feat: add input listener for live preview

--- algorithms/markdown-converter/script.js
+++ algorithms/markdown-converter/script.js
@@ -1,3 +1,9 @@
 const markdownInput = document.getElementById("markdown-input");
 const rawOutput = document.getElementById("html-output");
 const htmlPreview = document.getElementById("preview");
+
+
+markdownInput.addEventListener("input", () => {
+    rawOutput.textContent = markdownInput.value;
+    convertMarkdown();
+})
