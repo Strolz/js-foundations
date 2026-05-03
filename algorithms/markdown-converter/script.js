6c6eaa0118be7dbd46edd80ab535a3f2bb5030f9
Anton Strolz <tonystrolz@gmail.com>
2026-05-03 17:40:10 -0500

    feat: Implement and test first regexp for <h1> element converter

--- algorithms/markdown-converter/script.js
+++ algorithms/markdown-converter/script.js
@@ -2,11 +2,17 @@ const markdownInput = document.getElementById("markdown-input");
 const rawOutput = document.getElementById("html-output");
 const htmlPreview = document.getElementById("preview");
 
+const h1Exp = /^#/;
+
 function convertMarkdown() {
-    htmlPreview.innerHTML = `<h1>${markdownInput.value}</h1>`
-}
+    if(h1Exp.test(markdownInput.value)) {
+        rawOutput.textContent = `<h1>${markdownInput.value.slice(1)}</h1>`;
+        htmlPreview.innerHTML = `<h1>${markdownInput.value.slice(1)}</h1>`
+    }     
+}   
 
 markdownInput.addEventListener("input", () => {
     rawOutput.textContent = markdownInput.value;
+    htmlPreview.textContent = markdownInput.value;
     convertMarkdown();
 })
