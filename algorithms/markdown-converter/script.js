a6d02a45feb2d95e71d54f141110819aa2ccb95c
Anton Strolz <tonystrolz@gmail.com>
2026-05-03 16:05:18 -0500

    feat: mirror raw markdown to output panel

--- algorithms/markdown-converter/script.js
+++ algorithms/markdown-converter/script.js
@@ -2,6 +2,9 @@ const markdownInput = document.getElementById("markdown-input");
 const rawOutput = document.getElementById("html-output");
 const htmlPreview = document.getElementById("preview");
 
+function convertMarkdown() {
+    htmlPreview.innerHTML = `<h1>${markdownInput.value}</h1>`
+}
 
 markdownInput.addEventListener("input", () => {
     rawOutput.textContent = markdownInput.value;
