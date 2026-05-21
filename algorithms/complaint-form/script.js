44576d783f4595f2156f0847c1ac24c5ad822882
Anton Strolz <tonystrolz@gmail.com>
2026-05-20 20:49:50 -0500

    chore: complete caching all required DOM elements

--- algorithms/complaint-form/script.js
+++ algorithms/complaint-form/script.js
@@ -1,5 +1,29 @@
 const mainForm = document.getElementById("form");
 
+const personalInfo = document.getElementById("personal-info");
+const fullName = document.getElementById("full-name");
+const email = document.getElementById("email");
+
+const productInfo = document.getElementById("product-info");
+const orderNo = document.getElementById("order-no");
+const productCode = document.getElementById("product-code");
+const quantity = document.getElementById("quantity");
+
+const complaintsGroup = document.getElementById("complaints-group");
+const complaintCheckBox = document.querySelectorAll("#complaints-group input[type='checkbox']");
+
+const complaintDescriptionContainer = document.getElementById("complaint-description-container");
+const complaintDescription = document.getElementById("complaint-description");
+
+const solutionsGroup = document.getElementById("solutions-group");
+const solutionsRadioBtn = document.querySelectorAll("#solutions-group input[type='radio']");
+
+const solutionDescriptionContainer = document.getElementById("solution-description-container");
+const solutionDescription = document.getElementById("solution-description");
+
+const btnContainer = document.getElementById("btn-container");
+const submitBtn = document.getElementById("submit-btn");
+
 
 function validateForm() {
 
