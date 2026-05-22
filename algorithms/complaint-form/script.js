515360ea569534b2569fc2325d1df7c8e4b82992
Anton Strolz <tonystrolz@gmail.com>
2026-05-22 18:39:17 -0500

    feat: complete validateForm function checking all fields for truthy

--- algorithms/complaint-form/script.js
+++ algorithms/complaint-form/script.js
@@ -10,6 +10,7 @@ const productCode = document.getElementById("product-code");
 const quantity = document.getElementById("quantity");
 
 const complaintsGroup = document.getElementById("complaints-group");
+const otherComplaint = document.getElementById("other-complaint");
 const complaintCheckBox = document.querySelectorAll("#complaints-group input[type='checkbox']");
 
 const complaintDescriptionContainer = document.getElementById("complaint-description-container");
@@ -17,6 +18,7 @@ const complaintDescription = document.getElementById("complaint-description");
 
 const solutionsGroup = document.getElementById("solutions-group");
 const solutionsRadioBtn = document.querySelectorAll("#solutions-group input[type='radio']");
+const otherSolution = document.getElementById("other-solution");
 
 const solutionDescriptionContainer = document.getElementById("solution-description-container");
 const solutionDescription = document.getElementById("solution-description");
@@ -24,18 +26,22 @@ const solutionDescription = document.getElementById("solution-description");
 const btnContainer = document.getElementById("btn-container");
 const submitBtn = document.getElementById("submit-btn");
 
-
 function validateForm() {
 
      return {
-        "full-name": fullName.value,
-        "email": email.value.match(/@/),
-        "order-no": orderNo.value,
-        "product-code": productCode.value,
-        "quantity": quantity.value,
+        "full-name": !!fullName.value,
+        "email": !!email.value.match(/@/),
+        "order-no": !!orderNo.value,
+        "product-code": !!productCode.value,
+        "quantity": !!quantity.value,
         "complaints-group": Array.from(complaintCheckBox).some(box => box.checked),
-        "complaint-description": complaintDescription.value.length < 20,
+        "complaint-description": 
+            !otherComplaint.checked ||
+            complaintDescription.value.trim().length >= 20,
+        "solutions-group": Array.from(solutionsRadioBtn).some(r => r.checked),
+        "other-solution": 
+            !otherSolution.checked ||
+            solutionDescription.value.trim().length >= 20,
     }
-
     
 }
