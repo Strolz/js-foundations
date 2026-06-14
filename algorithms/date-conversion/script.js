36be88d87fd21cc7577001fc85d8a656a0a22ac4
Anton Strolz <tonystrolz@gmail.com>
2026-06-14 00:00:54 -0500

    feat: implement formatDate function to return standard MM/DD/YYYY

--- algorithms/date-conversion/script.js
+++ algorithms/date-conversion/script.js
@@ -2,3 +2,9 @@ const currentDate = new Date();
 const currentDateFormat = `Current Date and Time: ${currentDate.toString()}`;
 
 console.log(currentDateFormat);
+
+function formatDateMMDDYYYY(date) {
+  return `Formatted Date (MM/DD/YYYY): ${date.toLocaleDateString()}`;
+}
+
+console.log(formatDateMMDDYYYY(currentDate));
