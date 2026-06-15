01971c97fa89ae36787eabd8dec379194f80ed88
Anton Strolz <tonystrolz@gmail.com>
2026-06-14 19:04:58 -0500

    chore: Refine and add intention-driven JS comments

--- algorithms/date-conversion/script.js
+++ algorithms/date-conversion/script.js
@@ -1,5 +1,22 @@
+/*
+
+1. You should have a variable named currentDate that holds the current date and time using the Date object.
+2. You should have a variable named currentDateFormat that holds the current date in the format Current Date and Time: <ddd> <MMM> <dd> <yyyy> <HH>:<mm>:<ss> <TIMEZONE>.
+3. You should log the value of currentDateFormat to the console.
+4. You should have a function formatDateMMDDYYYY
+5. The function formatDateMMDDYYYY should take a single parameter.
+6. When the date object holds Fri Sep 27 2024 16:16:11 GMT+0500 (Pakistan Standard Time), the function formatDateMMDDYYYY should return Formatted Date (MM/DD/YYYY): 9/27/2024.
+7. You should have a function formatDateLong
+8. The function formatDateLong should take a single a parameter.
+9. When the date object holds Fri Sep 27 2024 16:16:11 GMT+0500 (Pakistan Standard Time), the function formatDateLong should return Formatted Date (Month Day, Year): September 27, 2024.
+
+ */
+
+// Stage curent date and time 
 const currentDate = new Date();
 const currentDateFormat = `Current Date and Time: ${currentDate.toString()}`;
+
+// Set accessible date format
 const options = {
   month: "long",
   day: "numeric",
@@ -8,10 +25,12 @@ const options = {
 
 console.log(currentDateFormat);
 
+// Return date in a clear month/day/year format
 function formatDateMMDDYYYY(date) {
   return `Formatted Date (MM/DD/YYYY): ${date.toLocaleDateString()}`;
 }
 
+// Return date using the long-form options 
 function formatDateLong(date) {
  return `Formatted Date (Month Day, Year): ${date.toLocaleDateString("en-US", options)}`;
 }
