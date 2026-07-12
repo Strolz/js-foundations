11134bcc61c1d583bbb282a3882f2e589f0d041d
Anton Strolz <tonystrolz@gmail.com>
2026-07-11 23:22:29 -0500

    feat: assign keydown listener to the document for each corresponding key Q, W, E, A, S, D, Z, X, C; all tests passing

--- algorithms/drum-machine/script.js
+++ algorithms/drum-machine/script.js
@@ -8,3 +8,15 @@ pads.forEach(pad => {
     display.innerText = pad.id;
     })
 })
+
+document.addEventListener("keydown", (e) => {
+    const key = e.key.toUpperCase();
+    const pad = document.querySelector(`.drum-pad:has(#${key})`);
+
+    if (pad) {
+        const audio = pad.querySelector("audio");
+        audio.currentTime = 0;
+        audio.play();
+        display.innerText = pad.id;
+    }
+})
