106d9aa91a34a85c870c75eeb5c9fcdeacab6a5a
Anton Strolz <tonystrolz@gmail.com>
2026-07-11 22:43:03 -0500

    feat: complete pad forEach loop adding click listeners to each pad and playing linked audio source when clicked

--- algorithms/drum-machine/script.js
+++ algorithms/drum-machine/script.js
@@ -1,2 +1,10 @@
 const pads = document.querySelectorAll(".drum-pad");
 const display = document.getElementById("display");
+
+pads.forEach(pad => {
+    const audio = pad.querySelector("audio");
+    pad.addEventListener("click", () => {
+    audio.play();
+    display.innerText = pad.id;
+    })
+})
