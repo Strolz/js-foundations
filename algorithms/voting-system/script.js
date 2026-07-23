52ad942832628915899129ef8a932a1a6d475450
Anton Strolz <tonystrolz@gmail.com>
2026-07-22 21:40:24 -0500

    feat: create 3 objects named optionA, optionB, & optionC and set each one to the poll map variable

--- algorithms/voting-system/script.js
+++ algorithms/voting-system/script.js
@@ -1,4 +1,20 @@
+const optionA = {
+  name: "Turkey"
+}
+
+const optionB = {
+  name: "Beef"
+}
+
+const optionC = {
+  name: "Ham"
+}
+
 const poll = new Map();
+
+poll.set(optionA, { votes: 1});
+poll.set(optionB, { votes: 3});
+poll.set(optionC, { votes: 5});
 
 const addOption = (option) => {
 }
