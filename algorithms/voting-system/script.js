a9f07361ec98372aeb132a0053f143623a43224f
Anton Strolz <tonystrolz@gmail.com>
2026-07-25 21:18:13 -0500

    feat: create addOption logic to check for empty / existing option and then add to poll

--- algorithms/voting-system/script.js
+++ algorithms/voting-system/script.js
@@ -17,6 +17,14 @@ poll.set("Beef", new Set());
 poll.set("Ham", new Set());
 
 const addOption = (option) => {
+  if(!option) {
+    return `Option cannot be empty`
+  }
+  if(option.has(option)) {
+    return `Option "${option}" already exists.`
+  }
+  poll.set(option, new Set()); 
+  return `Option "${option}" added to the poll`
 }
 
 const vote = (option, voterId) => {
