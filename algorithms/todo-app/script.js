01069cc068571ecf07b7012d84a660e7fb40202a
Anton Strolz <tonystrolz@gmail.com>
2026-08-07 20:16:33 -0500

    feat: add updated-input check to closeTaskFormBtn logic
    - introduce formInputValuesUpdated to detect changes to title, date, or description
    - update close condition to require both existing input values and actual edits

--- algorithms/todo-app/script.js
+++ algorithms/todo-app/script.js
@@ -90,9 +90,17 @@ openTaskFormBtn.addEventListener("click", () => {
 });
 
 closeTaskFormBtn.addEventListener("click", () => {
-    const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
+    const formInputsContainValues = 
+        titleInput.value || 
+        dateInput.value || 
+        descriptionInput.value;
 
-    if (formInputsContainValues) {
+    const formInputValuesUpdated = 
+        titleInput.value !== currentTask.title || 
+        dateInput.value !== currentTask.date ||
+        descriptionInput.value !== currentTask.description
+
+    if (formInputsContainValues && formInputValuesUpdated) {
     confirmCloseDialog.showModal();
     } else {
     reset();
