fbd04db325fc15c161e47bd919e5ec47bfb0bc83
Anton Strolz <tonystrolz@gmail.com>
2026-08-14 21:31:00 -0500

    chore: cache DOM elements

---
+++ algorithms/bookmark-app/script.js
@@ -0,0 +1,17 @@
+const categoryDropdown = document.getElementById("category-dropdown");
+const viewCategoryBtn = document.getElementById("view-category-button");
+const addBookmarkBtn = document.getElementById("add-bookmark-button");
+
+const formSection = document.getElementById("form-section");
+const listSection = document.getElementById("bookmark-list-section");
+
+const nameInput = document.getElementById("name");
+const urlInput = document.getElementById("url");
+
+const closeFormBtn = document.getElementById("close-form-button");
+const addBookmarkFormBtn = document.getElementById("add-bookmark-button-form");
+
+const categoryList = document.getElementById("category-list");
+const closeListBtn = document.getElementById("close-list-button");
+const deleteBookmarkBtn = document.getElementById("delete-bookmark-button");
+
