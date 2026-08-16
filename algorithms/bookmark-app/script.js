be3173638a30303d614da431f715a71d6836c1e5
Anton Strolz <tonystrolz@gmail.com>
2026-08-15 22:50:54 -0500

    feat: create getBookmarks function and stage for logic returning an empty array as default

--- algorithms/bookmark-app/script.js
+++ algorithms/bookmark-app/script.js
@@ -17,6 +17,10 @@ const categoryList = document.getElementById("category-list");
 const closeListBtn = document.getElementById("close-list-button");
 const deleteBookmarkBtn = document.getElementById("delete-bookmark-button");
 
+const getBookmarks = () => {
+    return [];
+}
+
 const displayOrCloseForm = () => {
     formSection.classList.toggle("hidden");
     mainSection.classList.toggle("hidden");
