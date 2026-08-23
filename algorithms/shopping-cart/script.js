d16a3473130db324d7d8a7ac78997f9008d75367
Anton Strolz <tonystrolz@gmail.com>
2026-08-22 22:48:05 -0500

    chore: begin to cache DOM elements

---
+++ algorithms/shopping-cart/script.js
@@ -0,0 +1,3 @@
+const cartContainer = document.getElementById("cart-container");
+const productsContainer = document.getElementById("products-container");
+const dessertContainer = document.getElementById("dessert-card-container");
