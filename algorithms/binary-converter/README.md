a28a05f2c1bcb61b702075ac67656984267dc242
Anton Strolz <tonystrolz@gmail.com>
2026-09-08 17:01:07 -0500

    feat: clear decimalToBinary function body - declare binary variable and assign empty string to stage

--- algorithms/binary-converter/README.md
+++ algorithms/binary-converter/README.md
@@ -315,7 +315,11 @@ Within the body of the if statement, set the innerText property of result equal 
 
 Step 36
 =======
+Now your decimalToBinary function is complete. Feel free to play around with it.
 
+But there are some ways to improve it. For example, it's not necessary to keep track of the inputs and quotients. We can clean things up so the function is more efficient.
+
+First, remove everything in the body of the decimalToBinary function. Then, use let to create a variable named binary and assign it an empty string.
 ---------------------------------
 
 Step 37
--- algorithms/binary-converter/script.js
+++ algorithms/binary-converter/script.js
@@ -3,34 +3,8 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-    let inputs = [];
-    let quotients = [];
-    let remainders = [];
-
-    if(input === 0) {
-        result.innerText = "0";
-        return
-    }
-
-    while(input > 0) {
-        const quotient = Math.floor(input / 2);      
-        const remainder = input % 2;
-
-        inputs.push(input);
-        quotients.push(quotient);
-        remainders.push(remainder);
-
-        input = quotient;
-    }
-
-    console.log("Inputs: ", inputs);
-    console.log("Quotients: ", quotients);
-    console.log("Remainders: ", remainders);
-
-    result.innerText = remainders
-        .reverse()
-        .join("");
-};
+  let binary = "";
+}
 
 const checkUserInput = () => {
     if (
