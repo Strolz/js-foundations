ce6a3d32b9f7a75291ce78c838de3ac3666d8560
Anton Strolz <tonystrolz@gmail.com>
2026-09-12 23:34:00 -0500

    feat: use getElementById with obj.inputVal to select target element and remove from DOM

--- algorithms/binary-converter/README.md
+++ algorithms/binary-converter/README.md
@@ -821,7 +821,7 @@ Add a setTimeout() function to your .forEach() loop. Pass in an empty callback f
 
 Step 106
 ========  
-
+Use the .getElementById() method to target the element with the id attribute with the value of the inputVal property of the current object. Then, use the .remove() method on that element to remove it from the DOM after the delay.
 ---------------------------------
 
 Step 107
--- algorithms/binary-converter/script.js
+++ algorithms/binary-converter/script.js
@@ -49,13 +49,15 @@ const showAnimation = () => {
     }, obj.addElDelay);
 
     setTimeout(() => {
-      document.getElementById(obj.inputVal)
-      .textContent = obj.msg;
+        document.getElementById(obj.inputVal)
+        .textContent = obj.msg;
     }, obj.showMsgDelay);
 
     setTimeout(() => {
-    
+        document.getElementById(obj.inputVal)
+        .remove();  
     }, obj.removeElDelay);
+
   });
 };
 
