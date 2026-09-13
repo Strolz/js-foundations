90d1333caaa90f237b827d88a4fe828343a78048
Anton Strolz <tonystrolz@gmail.com>
2026-09-13 15:50:26 -0500

    feat: iterate over string characters to build recursive branches

--- algorithms/permutation-generator/permutation-generator.js
+++ algorithms/permutation-generator/permutation-generator.js
@@ -1,6 +1,13 @@
 const permuteString = (str, prefix = "", results = [] ) => {
-  if(str === "") {
-    results.push(prefix)
-    return results;
-  };
+    if(str === "") {
+        results.push(prefix)
+        return results;
+    };
+
+    for (let i = 0; i < str.length; i++) {
+        const currentChar = str[i];
+        const newPrefix = prefix + currentChar;
+        const newRemaining = str.slice(0, i) + str.slice(i + 1);
+    }
+
 }
