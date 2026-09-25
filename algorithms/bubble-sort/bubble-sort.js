f2c6ad2c6cf6f093f4643c709f6b8976c7649945
Anton Strolz <tonystrolz@gmail.com>
2026-09-24 19:02:00 -0500

    feat: declare swapped variable and set to false within while loop to control flow - add for loop, set i to 0 and loop while i is less then the provided array length

--- algorithms/bubble-sort/bubble-sort.js
+++ algorithms/bubble-sort/bubble-sort.js
@@ -2,7 +2,11 @@ function bubbleSort(array) {
     let swapped = true;
 
     while(swapped) {
-    
+        swapped = false
+
+        for (let i = 0; i < array.length; i++) {
+      
+        }
     }
 
 }
