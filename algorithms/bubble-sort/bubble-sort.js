1ed0cfd7fd4796c8a5ce62980335b164d2a63a1e
Anton Strolz <tonystrolz@gmail.com>
2026-09-24 21:29:05 -0500

    feat: update for loop to check for array.length -1

--- algorithms/bubble-sort/bubble-sort.js
+++ algorithms/bubble-sort/bubble-sort.js
@@ -4,7 +4,7 @@ function bubbleSort(array) {
     while(swapped) {
         swapped = false
 
-        for (let i = 0; i < array.length; i++) {
+        for (let i = 0; i < array.length - 1; i++) {
             if(array[i] > array[i+1]) {
                 [array[i], array[i+1]] = [array[i+1], array[i]];
                 swapped = true;
