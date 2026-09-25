eaf6acc1cad9e098fdc21df61fd567f31e4f249f
Anton Strolz <tonystrolz@gmail.com>
2026-09-25 18:10:27 -0500

    chore: add intention driven comments

--- algorithms/selection-sort/selection-sort.js
+++ algorithms/selection-sort/selection-sort.js
@@ -1,13 +1,16 @@
 function selectionSort(arr) {
+    // store the current value before overwriting it
     for(let i = 0; i < arr.length - 1; i++) {
         let min = i;
-    
+
+        // place the smallest found value into the current position
         for(let j = i + 1; j < arr.length; j++) {
             if(arr[j] < arr[min]) {
             min = j;
             }
         }
 
+    // move the original value into the minIndex position
     let temp = arr[i];
     arr[i] = arr[min];
     arr[min] = temp;
